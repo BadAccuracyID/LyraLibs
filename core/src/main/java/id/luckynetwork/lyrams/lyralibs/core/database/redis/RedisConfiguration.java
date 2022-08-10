b5add2b232681f8b0a586ec543759bcfe8069c66
BadAccuracyID <37508513+BadAccuracyID@users.noreply.github.com>
2022-08-11 01:30:43 +0700

    Added default values and docs for RedisConfiguration

--- core/src/main/java/id/luckynetwork/lyrams/lyralibs/core/database/redis/RedisConfiguration.java
+++ core/src/main/java/id/luckynetwork/lyrams/lyralibs/core/database/redis/RedisConfiguration.java
@@ -3,20 +3,38 @@ package id.luckynetwork.lyrams.lyralibs.core.database.redis;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
 
 @Getter
 @Setter
 @Builder(builderMethodName = "newBuilder")
 public class RedisConfiguration {
-    private final String host;
-    private final int port;
 
-    private final int timeout;
+    /*
+     * The redis host
+     */
+    private final String host = "localhost";
+    /*
+     * The redis host's port
+     */
+    private final int port = 6379;
+    /*
+     * timeout before a connection is considered failed
+     */
+    private final int timeout = 30_000;
 
-    private final boolean auth;
-    private final String password;
+    /*
+     * the redis password, set to null if not needed
+     */
+    @Nullable
+    private final String password = null;
+    /*
+     * the client name
+     */
+    private final String clientName = "RedisClient";
+    /*
+     * the redis channel to use for pub/sub
+     */
+    private final String channel = "RedisChannel";
 
-    private final String clientName;
-
-    private final String channel;
 }
